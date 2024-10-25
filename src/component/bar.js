185a3f17a3c5a100f5dbdce2e2caf00cadd072c9
mingz-x <minji052400@gmail.com>
2024-10-25 18:13:30 +0900

    25-2

--- src/component/bar.js
+++ src/component/bar.js
@@ -5,11 +5,11 @@ const Bar = () => {
   return (
     <div className='art'>
         <div className="bar">
-            <Link href='/search'><img src='./search.svg'/></Link>
-            <Link href='/menu'><img src='./menu.svg'/></Link>
-            <Link href='/'><img src='./home.svg'/></Link>
-            <Link href='/scrap'><img src='./favorite.svg'/></Link>
-            <Link href='/my'><img src='./person.svg'/></Link>
+            <Link href='/search'><img src='search.svg'/></Link>
+            <Link href='/menu'><img src='menu.svg'/></Link>
+            <Link href='/'><img src='home.svg'/></Link>
+            <Link href='/scrap'><img src='favorite.svg'/></Link>
+            <Link href='/my'><img src='person.svg'/></Link>
         </div>
     </div>
   )
--- src/pages/login.js
+++ src/pages/login.js
@@ -16,17 +16,10 @@ console.log('sdfsdf')
             <h2>로그인</h2>
           </div>
           
-          
           <div className='sslogin'>
             <h2>소셜 계정으로 로그인</h2>
-            <button onClick={() => signIn('naver',{callbackUrl:'/'})} className='N'>
-              네이버 아이디로 로그인
-            </button>
-            
-            <button onClick={() => signIn('kakao',{callbackUrl:'/'})} className='K'>
-              카카오 아이디로 로그인
-            </button>
-
+            <button onClick={() => signIn('naver',{callbackUrl:'/'})} className='N'>네이버 아이디로 로그인</button>
+            <button onClick={() => signIn('kakao',{callbackUrl:'/'})} className='K'>카카오 아이디로 로그인</button>
             {/* <button onClick={()=>signIn('github',{callbackUrl:'/'})}></button> */}
           </div>
         </div>
